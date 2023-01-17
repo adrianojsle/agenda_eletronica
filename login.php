dff9c203eedc6b9d6d3c11c27795f07f542f6784
adrianojsle <oadrianoleite@gmail.com>
2023-01-17 12:36:26 -0300

    Criação do projeto

---
+++ login.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Login - Agenda Logos</title>
+    <!-- Bootstrap CSS para estilização -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+</head>
+
+<body>
+    <div class="container">
+        <div class="text-center mt-5">
+            <img src="logo.png" class="img-fluid" alt="Logo da empresa">
+            <h1 class="mb-5 text-primary">Login</h1>
+            <form>
+                <div class="form-group">
+                    <label for="username">Nome de usuário</label>
+                    <input type="text" class="form-control" id="username" placeholder="Insira seu nome de usuário">
+                </div>
+                <div class="form-group">
+                    <label for="password">Senha</label>
+                    <input type="password" class="form-control" id="password" placeholder="Insira sua senha">
+                </div>
+                <button type="submit" class="btn btn-primary btn-block">Entrar</button>
+            </form>
+        </div>
+    </div>
+    <!-- Bootstrap JS para estilização -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+</body>
+
+</html>
