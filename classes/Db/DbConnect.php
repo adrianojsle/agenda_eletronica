7d6b64a31bc3f0046da00c0924b0537b3faf53d3
adrianojsle <oadrianoleite@gmail.com>
2023-01-17 18:13:36 -0300

    Conexão com banco de dados e estrutura MVC

---
+++ classes/Db/DbConnect.php
@@ -0,0 +1,21 @@
+<?php
+
+class DbConnect {
+    private $host = 'localhost';
+    private $user = 'root';
+    private $password = '';
+    private $dbname = 'agenda_db';
+
+    public function connect() {
+        try {
+            $mysql_connect_str = "mysql:host=$this->host;dbname=$this->dbname";
+            $dbConnection = new PDO($mysql_connect_str, $this->user, $this->password);
+            $dbConnection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            return $dbConnection;
+        } catch (PDOException $e) {
+            echo "Error: " . $e->getMessage();
+        }
+    }
+}
+
+?>
