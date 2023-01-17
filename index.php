af4d665910c73ce2b34bafc16e557910d9375a56
adrianojsle <oadrianoleite@gmail.com>
2023-01-17 20:26:47 -0300

    Melhoria em segurança nas rotas

--- index.php
+++ index.php
@@ -1,19 +1,18 @@
 <?php
-    session_start();
-    require_once 'classes/Controller/SessionController.php';
-    $session = new SessionController();
-    if ($session->isLoggedIn()) {
-        require_once 'pages/dashboard.php';
-    } else {
-        if (isset($_GET['p'])) {
-            $page = $_GET['p'];
-            if (file_exists("pages/$page.php")) {
-                include "pages/$page.php";
-            } else {
-                require_once 'pages/err.php';
-            }
+session_start();
+require_once 'classes/Controller/SessionController.php';
+$session = new SessionController();
+if (isset($_GET['p'])) {
+    $page = $_GET['p'];
+    if (file_exists("pages/$page.php")) {
+        if ($session->isLoggedIn()) {
+            include "pages/$page.php";
         } else {
             require_once 'pages/login.php';
         }
+    } else {
+        require_once 'pages/err.php';
     }
-?>
+} else {
+    header("Location: /?p=login");
+}
